cdf275f3d74c61c60803d53d68a78e8be58ff684
ShacharHarshuv <shachar.harshuv@gmail.com>
2019-08-01 23:06:46 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import {
+  Platform,
+  ToastController
+} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AdMobFree } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +16,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
+    private adMob: AdMobFree,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private toasterController: ToastController
   ) {
     this.initializeApp();
   }
@@ -22,6 +28,22 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.adMob.banner.config({
+        autoShow: true,
+        isTesting: true
+      });
+      this.adMob.banner.prepare()
+        .then(() => {
+          // banner Ad is ready
+          // if we set autoShow to false, then we will need to call the show method here
+        })
+        .catch(async (e) => {
+          const toaster = await this.toasterController.create({
+            message: e,
+            color: 'danger'
+          });
+          toaster.present();
+        });
     });
   }
 }
